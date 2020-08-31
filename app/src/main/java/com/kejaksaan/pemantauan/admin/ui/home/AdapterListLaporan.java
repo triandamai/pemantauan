251d59387196bc58de83a3f37a85ca711e164042
Trian <triannurizkillah@gmail.com>
2020-09-01 04:46:40 +0700

    adding more

---
+++ app/src/main/java/com/kejaksaan/pemantauan/admin/ui/home/AdapterListLaporan.java
@@ -0,0 +1,56 @@
+package com.kejaksaan.pemantauan.admin.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kejaksaan.pemantauan.R;
+import com.kejaksaan.pemantauan.databinding.ItemLaporanBinding;
+import com.kejaksaan.pemantauan.databinding.ItemListPresensiBinding;
+import com.tdn.domain.model.AbsensiNama;
+import com.tdn.domain.model.LaporanModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterListLaporan extends RecyclerView.Adapter<AdapterListLaporan.MyViewHolder> {
+    private List<LaporanModel> data = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemLaporanBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_laporan, parent, false);
+        return new MyViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.binding.tvNama.setText(data.get(position).getAlamatTinggal());
+    }
+
+    public void setdata(List<LaporanModel> namaList) {
+        if (this.data == null) {
+            this.data = namaList;
+        } else {
+            this.data.addAll(namaList);
+        }
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return data == null ? 0 : data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private ItemLaporanBinding binding;
+
+        public MyViewHolder(@NonNull ItemLaporanBinding itemView) {
+            super(itemView.getRoot());
+            this.binding = itemView;
+        }
+    }
+}
--- app/src/main/java/com/kejaksaan/pemantauan/admin/ui/home/ListLaporanActivity.java
+++ app/src/main/java/com/kejaksaan/pemantauan/admin/ui/home/ListLaporanActivity.java
@@ -1,16 +1,37 @@
 package com.kejaksaan.pemantauan.admin.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import com.kejaksaan.pemantauan.R;
+import com.kejaksaan.pemantauan.databinding.ActivityListLaporanBinding;
+import com.tdn.domain.model.LaporanModel;
+
+import java.util.List;
 
 public class ListLaporanActivity extends AppCompatActivity {
+    private AdapterListLaporan adapterListLaporan;
+    private ListLaporanViewModel viewModel;
+    private ActivityListLaporanBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_laporan);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_list_laporan);
+        viewModel = new ViewModelProvider(this).get(ListLaporanViewModel.class);
+        adapterListLaporan = new AdapterListLaporan();
+        observe(viewModel);
+    }
+
+    private void observe(ListLaporanViewModel viewModel) {
+        viewModel.getLaporan().observe(this, laporanModels -> {
+            if (laporanModels != null) {
+                adapterListLaporan.setdata(laporanModels);
+            }
+        });
     }
 }
